24cfa9f01c4a8a836446a36104d34f1e6e7f5c3d
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-09-06 16:01:17 -0400

    remove unneccesary things

--- src/main/java/com/junyeong/yu/prototype/boot/BootApplication.java
+++ src/main/java/com/junyeong/yu/prototype/boot/BootApplication.java
@@ -1,13 +1,11 @@
 package com.junyeong.yu.prototype.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer; // this is for resolving jsp page
 
 @SpringBootApplication
-//@EnableAutoConfiguration
 public class BootApplication extends SpringBootServletInitializer {
 
 	@Override
